f05e1b863dea243062a41c9ea00cfcee53860571
Andrew-Wand <andrewwands@gmail.com>
2023-05-06 18:19:22 -0500

    Can now reset password

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase.config";
+import { sendPasswordResetEmail } from "firebase/auth";
+
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const [email, setEmail] = useState("");
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Email was sent!");
+    } catch (error) {
+      alert("Cannot send email");
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <p>Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+          <Link to="/sign-in">Sign In</Link>
+
+          <div>
+            <button>Send Reset Link</button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default ForgotPassword;
