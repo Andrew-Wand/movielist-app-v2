30d23649fff444bd592290632c6b0087c5dd7536
Andrew-Wand <andrewwands@gmail.com>
2023-05-14 19:52:09 -0500

    rating list now renders added movies

--- src/pages/RatingList.tsx
+++ src/pages/RatingList.tsx
@@ -11,44 +11,75 @@ import {
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db, auth } from "../../firebase.config";
+import Loading from "../components/Loading";
 
 function RatingList() {
+  const [ratinglist, setRatingList] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Fetch movies from firebase
+  const fetchRatingList = async () => {
+    try {
+      const ratinglistRef = collection(db, "ratingslist");
+      const q = query(
+        ratinglistRef,
+        where("userRef", "==", auth.currentUser?.uid)
+      );
+
+      const querySnap = await getDocs(q);
+
+      const ratinglistItems: any[] = [];
+
+      querySnap.docs.map((doc) => {
+        return ratinglistItems.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setRatingList(ratinglistItems);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRatingList();
+  }, [loading]);
+
   return (
-    <div className="overflow-x-auto p-2">
-      <table className="table table-zebra table-compact w-full">
-        {/* head */}
-        <thead>
-          <tr>
-            <th></th>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* row 1 */}
-          <tr>
-            <th></th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <th></th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <th></th>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-            <td>Red</td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      {loading ? (
+        <Loading />
+      ) : ratinglist && ratinglist?.length > 0 ? (
+        <div className="overflow-x-auto p-2">
+          <table className="table table-zebra table-compact w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Date</th>
+                <th>Rating</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {ratinglist?.map((item) => (
+                <tr>
+                  <th></th>
+                  <td>{item.data.movieName}</td>
+                  <td>{item.data.date}</td>
+                  <td>{item.data.rating}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div>Nothing here</div>
+      )}
     </div>
   );
 }
