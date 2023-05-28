3f72b39ba07c82c57324b41589afafba808446fb
Andrew-Wand <andrewwands@gmail.com>
2023-05-27 19:30:19 -0500

    navbar icons update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,33 +25,36 @@ function Header() {
   };
   return (
     <header>
-      <div className="text-6xl font-['Dancing_Script']">
+      <div className="text-6xl font-['Dancing_Script'] text-center m-5 flex justify-between">
         <h1>Movie Night</h1>
-      </div>
-      <div className="dropdown dropdown-end" onClick={openMenu}>
-        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
-            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </label>
-        <ul
-          tabIndex={0}
-          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          style={{ display: displayMenuStyle }}
+        <div
+          className="dropdown dropdown-end font-['sans-serif']"
+          onClick={openMenu}
         >
-          {auth.currentUser ? (
-            <li>
-              <Link to="/profile" className="justify-between">
-                Profile
-              </Link>
-              <a onClick={onLogout}>Logout</a>
-            </li>
-          ) : (
-            <li>
-              <Link to="/sign-in">Sign In</Link>
-            </li>
-          )}
-        </ul>
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-12 rounded-full">
+              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            style={{ display: displayMenuStyle }}
+          >
+            {auth.currentUser ? (
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                </Link>
+                <a onClick={onLogout}>Logout</a>
+              </li>
+            ) : (
+              <li>
+                <Link to="/sign-in">Sign In</Link>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     </header>
   );
--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -37,48 +37,64 @@ const MobileNavbar = (): JSX.Element => {
           >
             {list.id === 1 ? (
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
+                width="28px"
+                height="28px"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                stroke-width="1.6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                color="#fff"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
+                  d="M10 16h4M2 8l9.732-4.866a.6.6 0 01.536 0L22 8M20 11v8a2 2 0 01-2 2H6a2 2 0 01-2-2v-8"
+                  stroke="#fff"
+                  stroke-width="1.6"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
               </svg>
             ) : list.id === 2 ? (
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
+                width="28px"
+                height="28px"
+                stroke-width="1.6"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                color="#fff"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
+                  d="M15 21H9v-8.4a.6.6 0 01.6-.6h4.8a.6.6 0 01.6.6V21zM20.4 21H15v-2.9a.6.6 0 01.6-.6h4.8a.6.6 0 01.6.6v2.3a.6.6 0 01-.6.6zM9 21v-4.9a.6.6 0 00-.6-.6H3.6a.6.6 0 00-.6.6v4.3a.6.6 0 00.6.6H9zM10.806 5.113l.909-1.927a.312.312 0 01.57 0l.91 1.927 2.032.311c.261.04.365.376.176.568l-1.47 1.5.347 2.118c.044.272-.228.48-.462.351l-1.818-1-1.818 1c-.233.128-.506-.079-.462-.351l.347-2.118-1.47-1.5c-.19-.192-.085-.528.175-.568l2.034-.31z"
+                  stroke="#fff"
+                  stroke-width="1.6"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
               </svg>
             ) : list.id === 3 ? (
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
+                width="28px"
+                height="28px"
+                stroke-width="1.6"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                color="#fff"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                />
+                  d="M21.888 13.5C21.164 18.311 17.013 22 12 22 6.477 22 2 17.523 2 12S6.477 2 12 2c4.1 0 7.625 2.468 9.168 6"
+                  stroke="#fff"
+                  stroke-width="1.6"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <path
+                  d="M17 8h4.4a.6.6 0 00.6-.6V3"
+                  stroke="#fff"
+                  stroke-width="1.6"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
               </svg>
             ) : (
               ""
