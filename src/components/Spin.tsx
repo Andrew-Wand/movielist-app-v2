664edc60308fc13ac94c52cd703efda32bdaddcd
Andrew-Wand <andrewwands@gmail.com>
2023-05-31 14:09:38 -0500

    spin wheel added with css

---
+++ src/components/Spin.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import WheelComponent from "react-wheel-of-prizes";
+
+function Spin({ movielist }) {
+  const movieNames = movielist?.map((movie) => movie.data.movieName);
+  const segments = movieNames;
+  const segColors = [
+    "#EE4040",
+    "#F0CF50",
+    "#815CD1",
+    "#3DA5E0",
+    "#34A24F",
+    "#F9AA1F",
+    "#EC3F3F",
+    "#FF9000",
+    "#78B0A0",
+    "#81B214",
+    "#A64452",
+    "#6A097D",
+    "#295F4E",
+  ];
+
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
+
+  return (
+    <div className="wheel-wrapper">
+      <div className="wheel-text">
+        <h3>Spin the Wheel!</h3>
+      </div>
+      <div className="wheel-container">
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="#388cb3"
+          contrastColor="#0e141b"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={290}
+          upDuration={100}
+          downDuration={1000}
+          fontFamily="Staatliches"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Spin;
--- src/pages/SpinPage.tsx
+++ src/pages/SpinPage.tsx
@@ -1,5 +1,68 @@
+import Spin from "../components/Spin";
+import {
+  collection,
+  getDocs,
+  query,
+  doc,
+  where,
+  deleteDoc,
+  limit,
+  startAfter,
+  orderBy,
+  endBefore,
+  limitToLast,
+  DocumentData,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db, auth } from "../../firebase.config";
+import Loading from "../components/Loading";
+
 function SpinPage() {
-  return <div>SpinPage</div>;
+  const [movielist, setmovieList] = useState<any | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const movielistRef = collection(db, "movieslist");
+
+        const q = query(
+          movielistRef,
+          where("userRef", "==", auth.currentUser?.uid)
+        );
+
+        const querySnap = await getDocs(q);
+
+        const movielistItems: any[] = [];
+
+        querySnap.docs.map((doc) => {
+          return movielistItems.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setmovieList(movielistItems);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchMovies();
+  }, [loading]);
+
+  return (
+    <main>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Spin movielist={movielist} />
+        </div>
+      )}
+    </main>
+  );
 }
 
 export default SpinPage;
