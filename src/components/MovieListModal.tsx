93e273fe23008507f6006226a116be11b36d3f59
Andrew-Wand <andrewwands@gmail.com>
2023-09-12 18:57:44 -0500

    fix: fix for vercel to work

--- src/components/MovieListModal.tsx
+++ src/components/MovieListModal.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { auth, db } from "../../firebase.config";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { AiOutlinePlus } from "react-icons/ai";
 import { HiPlusCircle } from "react-icons/hi2";
 
 interface movieInfo {
